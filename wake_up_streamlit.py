b41e7ad49d1f125d25b01ab8e50c1cafdda29aa6
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-06-07 12:03:08 +0530

    Update wake_up_streamlit.py

--- wake_up_streamlit.py
+++ wake_up_streamlit.py
@@ -25,8 +25,7 @@ for url in STREAMLIT_APPS:
 
         # Find the button element by data-testid
         wakeup_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-testid='wakeup-button-owner']"))
-        )
+        EC.element_to_be_clickable((By.CLASS_NAME, "button_button_primary__E3Mmg")))
 
         # Click the button to wake up the Streamlit app
         wakeup_button.click()
