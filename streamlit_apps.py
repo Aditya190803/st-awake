44b7e93c9d9fd29c228ad4b5edc36a21f5869517
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-06-07 12:13:16 +0530

    Update streamlit_apps.py

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -2,4 +2,9 @@
 STREAMLIT_APPS = [
     "https://aditya190803-streamlit-example-streamlit-app-t7tdrs.streamlit.app/",
     "https://generate-a-blog.streamlit.app/",
+    "https://image-colorizer.streamlit.app/",
+    "https://chat-with-anything.streamlit.app/",
+    "https://csv-can-talk.streamlit.app/",
+    "https://jobfit.streamlit.app/",
+    "https://pdf-can-talk.streamlit.app/"
 ]
