c11d5fca0218de9aa99457ab99d894e70f6b5447
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-12-02 17:23:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
-# List of Streamlit App URLs
-
+# List of Streamlit app URLs
 STREAMLIT_APPS = [
-    "https://example1.streamlit.app/",
-    "https://example2.streamlit.app/",
-    "https://example3.streamlit.app/",
-    "https://example4.streamlit.app/",
-    "https://example5.streamlit.app/",
-    "https://example6.streamlit.app/",
-    "https://example7.streamlit.app/",
-    "https://example8.streamlit.app/",
-    "https://example9.streamlit.app/",
-    "https://example10.streamlit.app/"
+    "https://generate-a-blog.streamlit.app/",
+    "https://image-colorizer.streamlit.app/",
+    "https://chat-with-anything.streamlit.app/",
+    "https://csv-can-talk.streamlit.app/",
+    "https://jobfit.streamlit.app/",
+    "https://pdf-can-talk.streamlit.app/",
+    "https://generate-a-blog.streamlit.app/",
+    "https://llama-3-1.streamlit.app/",
+    "https://handwritten-digit-recognition-st.streamlit.app/",
+    "https://communicate-with-website.streamlit.app/",
+    "https://movie-recomendation-system-app.streamlit.app/"
 ]
